a85d5e64208f8e53491ef0738378a9f415db72f9
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-06-11 01:08:14 -0400

    Columns can be added to rows both existing and new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const grid = document.getElementById("grid");
 const rowInput = document.getElementById("row-input");
 const rowNumber = document.getElementById("row-number");
+const columnInput = document.getElementById("column-input");
+const columnNumber = document.getElementById("column-number");
 
 let rowsDisplayed = parseInt(rowInput.getAttribute("initial"));
-let rowsMax = parseInt(rowInput.getAttribute("max"));
-let rowsMin = parseInt(rowInput.getAttribute("min"));
-let columnsDisplayed = 5;
+const rowsMax = parseInt(rowInput.getAttribute("max"));
+const rowsMin = parseInt(rowInput.getAttribute("min"));
+let columnsDisplayed = parseInt(columnInput.getAttribute("initial"));
+const columnsMax = parseInt(columnInput.getAttribute("max"));
+const columnsMin = parseInt(columnInput.getAttribute("min"));
 
 rowNumber.innerHTML = rowsDisplayed;
+columnNumber.innerHTML = columnsDisplayed;
 
 const addRow = () => {
     if (!(rowsDisplayed === rowsMax)) {
@@ -27,5 +32,16 @@ const removeRow = () => {
         rowsDisplayed--;
         grid.removeChild(grid.lastChild);
         rowNumber.innerHTML = rowsDisplayed;
+    }
+}
+
+const addColumn = () => {
+    if (!(columnsDisplayed === columnsMax)) {
+        columnsDisplayed++;
+        for (row of grid.querySelectorAll("tr")) {
+            let newColumn = document.createElement("td");
+            row.append(newColumn);
+        }
+        columnNumber.innerHTML = columnsDisplayed;
     }
 }
