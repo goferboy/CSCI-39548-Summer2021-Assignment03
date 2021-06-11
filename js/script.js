6ef7b4f49b597f9abdfc428a2b8c461d307c8422
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-06-11 00:40:45 -0400

    Completely redo add row button

--- js/script.js
+++ js/script.js
@@ -1,35 +1,22 @@
-let rows;
-let columns;
-
 const grid = document.getElementById("grid");
 const rowInput = document.getElementById("row-input");
-const maxRow = parseInt(rowInput.getAttribute("max"));
+const rowNumber = document.getElementById("row-number");
 
-/*
-validateInput() is a helper function used for the rows and columns input field 
-to verify that given input was 0-9. paste from clipboard, backspace, and DEL key
-in order to properly display numeric integers valid with the input fields.
-*/
-const validateInput = (eventObj) => {
-    if (Number.isInteger(parseInt(eventObj.data)) ||        //tests to make sure input was an integer
-        eventObj.inputType === "insertFromPaste" ||         //allows pasting from clipboard
-        eventObj.inputType === "deleteContentForward" ||    //allows front deletion (DEL key)
-        eventObj.inputType === "deleteContentBackward" ||   //allows backspace deletion
-        !('inputType' in eventObj))                         //Prevents a Chrome Behavior in which clicking
-                                                            //up or down would clear the input field
-        return true;
-    else
-        return false;
-}
+let rowsDisplayed = parseInt(rowInput.getAttribute("initial"));
+let rowsMax = parseInt(rowInput.getAttribute("max"));
+let columnsDisplayed = 5;
 
-const rowModifier = (event) => {
-    if (validateInput(event)) {
-        if (rowInput.value > maxRow)
-            rowInput.value = maxRow.toString();
-        rows = parseInt(rowInput.value);
+rowNumber.innerHTML = rowsDisplayed;
+
+const addRow = () => {
+    if (!(rowsDisplayed === rowsMax)) {
+        rowsDisplayed++;
+        let newRow = document.createElement("tr");
+        for (let i = 0; i < columnsDisplayed; i++) {
+            let newColumn = document.createElement("td");
+            newRow.append(newColumn);
+        }
+        grid.append(newRow);
+        rowNumber.innerHTML = rowsDisplayed;
     }
-    else 
-        rowInput.value = rows;
 }
-
-document.getElementById("row-input").addEventListener('input', rowModifier);
